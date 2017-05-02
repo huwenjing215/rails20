b2fcb5eaff94ed08bfc2a75425ebd281227c9ad1
huwenjing <huwenjing215@gmail.com>
2017-05-02 15:31:14 +0800

    who create a group join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
 before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
 def index
@@ -15,6 +15,7 @@ def create
   @group = Group.new(group_params)
   @group.user = current_user
    if @group.save
+     current_user.join(@group)
   redirect_to groups_path
 else
   render :new
@@ -22,7 +23,6 @@ end
 end
 
 def edit
-
 end
 
 def update
@@ -44,6 +44,31 @@ def destroy
   flash[:alert] = "group delete"
 
 end
+
+def join
+  @group = Group.find(params[:id])
+
+  if !current_user.is_member_of?(@group)
+    current_user.join!(@group)
+    flash[:notice] = "Success to join"
+  else
+    flash[:warning] = "You have been the member~"
+  end
+  redirect_to group_path(@group)
+end
+
+def quit
+  @group = Group.find(params[:id])
+
+  if current_user.is_member_of?(@group)
+    current_user.quit!(@group)
+    flash[:notice] = "Quit the group"
+  else
+    flash[:warning] = "You don't have need to to quit~"
+  end
+  redirect_to group_path(@group)
+end
+
 
 private
 
